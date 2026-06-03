b0085522f78ee63fe2572a071630f4862cb42897
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-06-03 01:12:43 -0300

    notas agora tem LORE

--- scripts/note.gd
+++ scripts/note.gd
@@ -1,13 +1,11 @@
 class_name Note
 extends Node3D
 
-@export var note_texture: Texture2D
-
 
 func _ready() -> void:
 	add_to_group("interactable")
 
 
 func interact(_player) -> void:
-	NoteReader.collect(note_texture)
+	NoteReader.collect_next()
 	queue_free()
---
+++ scripts/note_item.gd
@@ -0,0 +1,6 @@
+class_name NoteItem
+extends Resource
+
+@export var text: String = ""
+@export var texture: Texture2D = null
+@export var is_tip: bool = false
